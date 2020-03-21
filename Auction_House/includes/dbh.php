1cb5c6c3c78a3541e3231e72333183965e97735c
nickb82 <nbollone@gmail.com>
2020-03-21 10:58:46 -0700

    Nick B added folders for program

---
+++ Auction_House/includes/dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Programmer386!"
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
